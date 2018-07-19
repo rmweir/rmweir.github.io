5426133ae8ae28774efad3bf3b7bc11cff2f883d
rmweir <Ricardo_Weir@dell.com>
2018-07-19 12:44:18 -0500

    add / route

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', function(req, res) {
+    res.send('Hey');
+});
+
 app.listen(3000, function() {
     console.log('yooo');
 });
